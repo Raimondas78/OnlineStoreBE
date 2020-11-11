cd27c46e1aa9767237b362ff223e836af0f3d84c
RaimondasU <u.raimondas@gmail.com>
2020-11-11 19:05:37 +0200

    update

---
+++ src/main/resources/db/migrations/V1.1.2__some_date.sql
@@ -0,0 +1,17 @@
+INSERT INTO category(name)
+VALUES ('Tapyba');
+
+INSERT INTO category(name)
+VALUES ('Medžio drožiniai');
+
+INSERT INTO category(name)
+VALUES ('Keramika');
+
+INSERT INTO category(name)
+VALUES ('Papuošalai');
+
+INSERT INTO category(name)
+VALUES ('Tekstilė');
+
+INSERT INTO category(name)
+VALUES ('Oda');
